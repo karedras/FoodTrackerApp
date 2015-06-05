03227986fa36f695d5023d72862a6db77ce63f65
Adriel Carsete <adrielcarsete@mac1733.local>
2015-06-05 14:04:49 -0300

    Initial Commit

---
+++ FoodTrackerApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTrackerApp
+//
+//  Created by Adriel Carsete on 6/5/15.
+//  Copyright (c) 2015 Adriel Carsete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
